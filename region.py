9ca886eeee4ff6b88d20d468d1b9f5469ce36023
fungiy <troynobell@gmail.com>
2018-08-22 23:53:09 +0900

    グラフを見比べるところまで実装

--- region.py
+++ region.py
@@ -11,10 +11,15 @@ from sklearn import neighbors
 
 from evaluation import LV1_Evaluator
 from labels import COLOR2ID, ID2COLOR
+from sampling import lv1_user_function_sampling_meshgrid_rectangular, lv1_user_function_sampling
 
 IMAGE_SIZE = 512
 CLASS_SIZE = 10
 DIVIDER = '------------------------'
+
+METHOD_NAME_REGION = 'lv1_user_function_sampling_region'
+METHOD_NAME_GRID = 'lv1_user_function_sampling_meshgrid_rectangular'
+METHOD_NAME_RANDOM = 'lv1_user_function_sampling'
 
 
 class LV1TargetClassifier:
@@ -60,6 +65,11 @@ class LV1UserDefinedClassifier:
     def predict(self, features):
         labels = self.clf.predict(features)
         return np.int32(labels)
+
+
+def create_dir(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
 
 
 class SavePathManager:
@@ -195,10 +205,10 @@ def create_region_map(features, target_labels, n, clone_labels):
     return polygon_list, list(survival_seg_set)
 
 
-def draw_segments(seg_list, draw_segments_save_dir):
+def draw_segments(seg_label_list, draw_segments_save_dir):
     create_dir(draw_segments_save_dir)
 
-    for seg, label in seg_list:
+    for seg, label in seg_label_list:
         point1, point2 = seg.points
         x1 = float(point1.x)
         y1 = float(point1.y)
@@ -265,9 +275,9 @@ def lv1_user_function_sampling_region(n_samples, target_model, exe_n, method_nam
     clone_labels = clone_model.predict(features=old_features)
 
     polygon_list, seg_list = create_region_map(features=old_features,
-                                     target_labels=target_labels,
-                                     clone_labels=clone_labels,
-                                     n=n_samples - 1)
+                                               target_labels=target_labels,
+                                               clone_labels=clone_labels,
+                                               n=n_samples - 1)
 
     point_undecided = True
 
@@ -282,14 +292,30 @@ def lv1_user_function_sampling_region(n_samples, target_model, exe_n, method_nam
             if polygon.encloses_point(point):  # 多角形の領域外の点なら点を再決定
                 point_undecided = True
 
-    draw_segments(seg_list, draw_segments_save_dir=path_manager.sampling_history_n_dir(exe_n=exe_n, method_name=method_name, sampling_n=n_samples-1))
+    draw_segments(seg_list,
+                  draw_segments_save_dir=path_manager.sampling_history_n_dir(exe_n=exe_n, method_name=method_name,
+                                                                             sampling_n=n_samples - 1))
 
     return np.vstack((old_features, new_features))
 
 
+def get_features(target, exe_n,
+                 method_name, path_manager):
+
+    if method_name == METHOD_NAME_REGION:
+        return lv1_user_function_sampling_region(n_samples=exe_n, target_model=target, exe_n=exe_n,
+                                                     method_name=method_name, path_manager=path_manager)
+
+    if method_name == METHOD_NAME_GRID:
+        return lv1_user_function_sampling_meshgrid_rectangular(n_samples=exe_n)
+
+    if method_name == METHOD_NAME_RANDOM:
+        return lv1_user_function_sampling(n_samples=exe_n)
+
+
 def exe_clone(target, exe_n, method_name, path_manager: SavePathManager):
     # ターゲット認識器への入力として用いる二次元特徴量を用意
-    features = lv1_user_function_sampling_region(n_samples=exe_n, target_model=target, exe_n=exe_n, method_name=method_name, path_manager=path_manager)
+    features = get_features(target=target, exe_n=exe_n, method_name=method_name, path_manager=path_manager)
 
     print(features)
     print(features.shape)
@@ -307,9 +333,11 @@ def exe_clone(target, exe_n, method_name, path_manager: SavePathManager):
     # 学習したクローン認識器を可視化し，精度を評価
     evaluator = LV1_Evaluator()
     visualize_save_dir = path_manager.sampling_method_dir(exe_n=exe_n, method_name=method_name)
+    create_dir(visualize_save_dir)
     evaluator.visualize(model, os.path.join(visualize_save_dir, 'visualize.png'))
     print('visualized')
-    evaluator.visualize_missing(model=model, target=target, filename=os.path.join(visualize_save_dir, 'visualize_miss.png'), features=features)
+    evaluator.visualize_missing(model=model, target=target,
+                                filename=os.path.join(visualize_save_dir, 'visualize_miss.png'), features=features)
     print("\nThe clone recognizer was visualized and saved to {0} .".format(visualize_save_dir))
     accuracy = evaluator.calc_accuracy(target, model)
     print("\naccuracy: {0}".format(accuracy))
@@ -318,7 +346,7 @@ def exe_clone(target, exe_n, method_name, path_manager: SavePathManager):
 
 
 def exe_clone_one():
-    n = 5
+    n = 10
     method_name = 'lv1_user_function_sampling_region'
 
     now_str = datetime.now().strftime('%Y%m%d%H%M%S')
@@ -331,9 +359,82 @@ def exe_clone_one():
     exe_clone(target=target, exe_n=n, method_name=method_name, path_manager=save_path_manager)
 
 
-def create_dir(path):
-    if not os.path.isdir(path):
-        os.makedirs(path)
+def exe_clone_all(range_arr, target, save_path_manager: SavePathManager, method_name):
+
+    n_list = []
+    acc_list = []
+    n_list.append(0)
+    acc_list.append(0.0)
+
+    for n in range_arr:
+        accuracy = exe_clone(target=target, exe_n=n, method_name=method_name, path_manager=save_path_manager)
+
+        n_list.append(n)
+        acc_list.append(accuracy)
+
+    return n_list, acc_list
+
+
+def save_and_show_graph(graph_dir, n_list, region_acc_list, grid_acc_list, random_acc_list):
+
+    print(DIVIDER)
+    print('n list')
+    print(n_list)
+    print('region_acc_list')
+    print(region_acc_list)
+    print(DIVIDER)
+
+    left = np.array(n_list)
+    region_acc_height = np.array(region_acc_list)
+    grid_acc_height = np.array(grid_acc_list)
+    random_acc_height = np.array(random_acc_list)
+    plt.plot(left, region_acc_height, label='region Accuracy')
+    plt.plot(left, grid_acc_height, label='grid Accuracy')
+    plt.plot(left, random_acc_height, label='random Accuracy')
+    plt.xlabel("n samples")
+    plt.ylabel("Accuracy")
+    plt.grid(True)
+    plt.ylim(0, 1)
+    plt.savefig(os.path.join(graph_dir, 'n_accuracy.png'))
+    plt.legend()
+    plt.show()
+    plt.close()
+
+
+def create_output():
+    now_str = datetime.now().strftime('%Y%m%d%H%M%S')
+    target_path = 'lv1_targets/classifier_01.png'
+    save_path_manager = SavePathManager(save_root_dir='output/' + now_str)
+
+    target = LV1TargetClassifier()
+    target.load(target_path)
+
+    range_arr = []
+    for i in range(0, 10):
+        range_arr.append(2**i)
+
+    print(DIVIDER)
+    print('実行間隔')
+    print(range_arr)
+    print(DIVIDER)
+
+    region_n_list, region_acc_list = exe_clone_all(range_arr=range_arr, target=target,
+                                                          save_path_manager=save_path_manager, method_name=METHOD_NAME_REGION)
+
+    grid_n_list, grid_acc_list = exe_clone_all(range_arr=range_arr, target=target,
+                                                   save_path_manager=save_path_manager, method_name=METHOD_NAME_GRID)
+
+    random_n_list, random_acc_list = exe_clone_all(range_arr=range_arr, target=target,
+                                               save_path_manager=save_path_manager, method_name=METHOD_NAME_RANDOM)
+
+    save_and_show_graph(
+        graph_dir=save_path_manager.save_root_dir,
+        n_list=region_n_list,
+        region_acc_list=region_acc_list,
+        grid_acc_list=grid_acc_list,
+        random_acc_list=random_acc_list
+    )
+
 
 if __name__ == '__main__':
-    exe_clone_one()
+    create_output()
