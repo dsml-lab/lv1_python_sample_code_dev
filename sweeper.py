306b232dab59fcf61a9645976cd76d6d86f88584
fungiy <troynobell@gmail.com>
2018-08-28 20:15:00 +0900

    デバブをかける

--- sweeper.py
+++ sweeper.py
@@ -3,14 +3,13 @@ import random
 
 
 np.set_printoptions(suppress=True)
-OPENED = 1000.
 LABEL_SIZE = 10
 
 
 def get_distribution(size=10):
     def calc(x, y):
         r = x ** 2 + y ** 2
-        return int(np.exp(-r) * 10 ** 2)
+        return np.exp(-r) * 10 ** 2
 
     xn = size
     x0 = np.linspace(-2, 2, xn)
@@ -18,7 +17,7 @@ def get_distribution(size=10):
     arr = np.zeros((len(x0), len(x1)))
     for i0 in range(xn):
         for i1 in range(xn):
-            arr[i1, i0] = calc(x0[i0], x1[i1])
+            arr[i0, i1] = calc(x0[i0], x1[i1])
 
     return arr
 
@@ -28,6 +27,7 @@ class Board:
     def __init__(self, board_size):
         self.board_size = board_size
         self.positions = np.zeros((LABEL_SIZE, board_size, board_size))
+        self.colorless_positions = np.zeros((board_size, board_size))
         self.sampling_points_all = np.full((board_size, board_size), True)
         self.max_position = board_size - 1
         self.integrate_positions = np.ones((board_size, board_size))
@@ -58,19 +58,22 @@ class Board:
 
     # 点を開示
     def open_once(self, x, y, color):
-
         # 近傍の点のx,yからの距離を算出
         distribution_arr = get_distribution(size=self.board_size * 2 + 1)  # 中心を最大の値として中心から遠ざかるほど値が小さくなる2次元配列を作る
         trimming_distribution_arr = distribution_arr[self.board_size - x:self.board_size * 2 - x,
                               self.board_size - y:self.board_size * 2 - y]
 
         self.positions[color] = self.positions[color] + trimming_distribution_arr
-
-        self.positions[color][x, y] += OPENED
         self.sampling_points_all[x, y] = False
 
     # 点を開示
     def open_once_colorless(self, x, y):
+        # 近傍の点のx,yからの距離を算出
+        distribution_arr = get_distribution(size=self.board_size * 2 + 1)  # 中心を最大の値として中心から遠ざかるほど値が小さくなる2次元配列を作る
+        trimming_distribution_arr = distribution_arr[self.board_size - x:self.board_size * 2 - x,
+                                    self.board_size - y:self.board_size * 2 - y]
+
+        self.colorless_positions = self.colorless_positions - trimming_distribution_arr
         self.sampling_points_all[x, y] = False
 
     def print(self):
@@ -87,6 +90,8 @@ class Board:
 
         for i in range(LABEL_SIZE):
             arr = np.absolute(arr - self.positions[i])
+
+        arr = np.absolute(arr - self.colorless_positions)
 
         max_value = np.amax(arr) + 1
         max_arr = np.full((self.board_size, self.board_size), max_value)
