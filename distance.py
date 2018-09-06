57e69d631421ab759fdf2711d2c711658e4abd47
satumaimo <fungiy@gmail.com>
2018-09-06 22:16:39 +0900

    サンプリング可能点から除外できてなかった問題を修正

--- distance.py
+++ distance.py
@@ -1,3 +1,5 @@
+import unittest
+
 import numpy as np
 import random
 
@@ -19,9 +21,63 @@ def find_furthest_place(sampled_features, filtered_samplable_features):
     for i, filtered_feature in enumerate(filtered_samplable_features):
         nearest_arr[i] = np.min(distance_arr[i])
 
+    print(nearest_arr)
+
     max_value = np.amax(nearest_arr)
+
+    print(max_value)
+
+    print("sampling候補数: " + str(len(nearest_arr)))
 
     index_list = np.where(max_value == nearest_arr)[0]
     random.shuffle(index_list)
 
+    print(index_list)
+
     return filtered_samplable_features[index_list[0]]
+
+
+class DistanceTest(unittest.TestCase):
+
+    def test_calc_distance(self):
+
+        feature1 = np.array([2, 3])
+        feature2 = np.array([4, 6])
+
+        result = calc_distance(feature1=feature1, feature2=feature2)
+
+        self.assertEqual(13, result)
+
+    def test_find_furthest_place(self):
+        feature1 = np.array([100, 99])
+        feature2 = np.array([4, 6])
+        feature3 = np.array([3, 6])
+
+        feature4 = np.array([100, 100])
+        feature5 = np.array([33, -55])
+
+        sampled_features = [feature1, feature2, feature3]
+        filtered_samplable_features = [feature4, feature5]
+
+        result = find_furthest_place(sampled_features=sampled_features, filtered_samplable_features=filtered_samplable_features)
+
+        self.assertEqual(0, result)
+
+    def test_find_furthest_place_(self):
+        sampled_features = [
+            np.array([1, 1]),
+            np.array([1, 1]),
+            np.array([1, 1]),
+            np.array([1, 1]),
+            np.array([1, 1]),
+            np.array([1, 1]),
+        ]
+        filtered_samplable_features = [
+            np.array([2, 2]),
+            np.array([4, 4]),
+            np.array([6, 6]),
+            np.array([8, 8]),
+        ]
+        result = find_furthest_place(sampled_features=sampled_features, filtered_samplable_features=filtered_samplable_features)
+
+        self.assertEqual(0, result)
