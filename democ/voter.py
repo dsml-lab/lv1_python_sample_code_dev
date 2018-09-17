7be7e64470d5291e6babd489fc362519d3785e2f
fungiy <troynobell@gmail.com>
2018-09-17 21:51:12 +0900

    Lv2では実数で優先度を比較するように変更

--- democ/voter.py
+++ democ/voter.py
@@ -1,8 +1,23 @@
 import numpy as np
+from abc import ABCMeta, abstractmethod
 from sklearn.preprocessing import OneHotEncoder
 
 
-class Lv1Voter:
+class Voter(metaclass=ABCMeta):
+    @abstractmethod
+    def sampled_fit(self, sampled_features, sampled_likelihoods):
+        pass
+
+    @abstractmethod
+    def samplable_predict(self, samplable_features):
+        pass
+
+    @abstractmethod
+    def get_samplable_likelihoods(self):
+        pass
+
+
+class Lv1Voter(Voter):
     """投票者クラス"""
 
     def __init__(self, model, label_size):
@@ -12,8 +27,8 @@ class Lv1Voter:
 
     # クローン認識器の学習
     #   (features, labels): 訓練データ（特徴量とラベルのペアの集合）
-    def sampled_fit(self, sampled_features, sampled_labels):
-        self.model.fit(sampled_features, sampled_labels)  # 学習
+    def sampled_fit(self, sampled_features, sampled_likelihoods):
+        self.model.fit(sampled_features, sampled_likelihoods)  # 学習
 
     # 未知の二次元特徴量を認識
     #   features: 認識対象の二次元特徴量の集合
@@ -24,22 +39,28 @@ class Lv1Voter:
         encoder = OneHotEncoder(self.label_size)
         return encoder.fit_transform(np.reshape(labels, (-1, 1))).toarray()
 
+    def get_samplable_likelihoods(self):
+        return self.samplable_labels
 
-class Lv2Voter:
+
+class Lv2Voter(Voter):
     """投票者クラス"""
 
     def __init__(self, model, label_size):
         self.model = model
-        self.samplable_labels = None
+        self.samplable_likelihoods = None
         self.label_size = label_size
 
     # クローン認識器の学習
     #   (features, labels): 訓練データ（特徴量とラベルのペアの集合）
-    def sampled_fit(self, sampled_features, sampled_labels):
-        self.model.fit(features=sampled_features, likelihoods=sampled_labels)  # 学習
+    def sampled_fit(self, sampled_features, sampled_likelihoods):
+        self.model.fit(features=sampled_features, likelihoods=sampled_likelihoods)  # 学習
 
     # 未知の二次元特徴量を認識
     #   features: 認識対象の二次元特徴量の集合
     def samplable_predict(self, samplable_features):
         likelihoods = self.model.predict_proba(samplable_features)
-        self.samplable_labels = np.int32(likelihoods >= 0.5)  # 予測結果を保持
+        self.samplable_likelihoods = likelihoods  # 予測結果を保持
+
+    def get_samplable_likelihoods(self):
+        return self.samplable_likelihoods
