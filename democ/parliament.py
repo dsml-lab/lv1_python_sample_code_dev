266361387907aaf0a9e1f3811a1a47f8d8fb14d3
satumaimo <fungiy@gmail.com>
2018-09-20 17:07:30 +0900

    仮想的なサンプリング済点を実装

--- democ/parliament.py
+++ democ/parliament.py
@@ -1,4 +1,5 @@
 import math
+import unittest
 
 import numpy as np
 
@@ -37,8 +38,36 @@ class Parliament:
             y = i // image_size
             samplable_features[i][0] = np.float32((x - h) / h) * 0.9
             samplable_features[i][1] = np.float32(-(y - h) / h) * 0.9
-
         return np.float32(samplable_features)
+
+    @staticmethod
+    def get_virtual_sampled_features_2_dimension(sampled_features):
+        dimension = 2
+        x_list = [-1.0, 1.0]
+        y_list = [-1.0, 1.0]
+
+        range_list = []
+        breadth = 10
+        for i in range(-breadth, breadth+1, 1):
+            range_list.append(i*(1/breadth))
+
+        print(len(range_list)*(dimension**2))
+        features = np.zeros((len(range_list)*(dimension**2), 2))
+
+        count = 0
+        for x in x_list:
+            for r in range_list:
+                features[count][0] = x
+                features[count][1] = r
+                count += 1
+
+        for y in y_list:
+            for r in range_list:
+                features[count][0] = r
+                features[count][1] = y
+                count += 1
+
+        return np.vstack((features, sampled_features))
 
     @staticmethod
     def create_lv1_voters():
@@ -60,6 +89,7 @@ class Parliament:
         self.samplable_features = samplable_features
 
     def get_optimal_solution(self, sampled_features, sampled_likelihoods):
+        sampled_features = self.get_virtual_sampled_features_2_dimension(sampled_features=sampled_features)
         self.__fit_to_voters(sampled_features=sampled_features, sampled_likelihoods=sampled_likelihoods)  # 投票者を訓練
         self.__predict_to_voters()  # 投票者による予測
 
@@ -98,3 +128,25 @@ class Parliament:
     def __predict_to_voters(self):
         self.voter1.samplable_predict(samplable_features=self.samplable_features)
         self.voter2.samplable_predict(samplable_features=self.samplable_features)
+
+
+class ParliamentTest(unittest.TestCase):
+    # def test__delete_samplable_features(self):
+    #     par = Parliament()
+    #     print(par.samplable_features[2])
+    #
+    #     par.delete_samplable_features(np.array([-0.9921875, 1]))
+    #
+    #     print(par.samplable_features[2])
+
+    def test_get_virtual_sampled_features_2_dimension(self):
+        voters = Parliament.create_lv1_voters()
+        p = Parliament(
+                dimension=2,
+                label_size=10,
+                samplable_features=Parliament.get_samplable_features_2_dimension(
+                    image_size=Parliament.get_image_size(exe_n=100)), voter1=voters[0], voter2=voters[1])
+
+        features = p.get_virtual_sampled_features_2_dimension()
+        print(features)
+        print(len(features))
