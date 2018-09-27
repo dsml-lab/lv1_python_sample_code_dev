f5634b1eaf03621cd44334b97373c69951e25990
fungiy <troynobell@gmail.com>
2018-09-27 15:03:41 +0900

    正しいラベルを返す

--- democ/lv2_clf.py
+++ democ/lv2_clf.py
@@ -35,3 +35,52 @@ class LV2UserDefinedClassifierMLP1000HiddenLayer:
             likelihoods = np.hstack([likelihoods, np.c_[p[:, 1]]])
         likelihoods = likelihoods[:, 1:]
         return np.float32(likelihoods)
+
+
+class LV2UserDefinedClassifierMLP1000HiddenLayerCorrectLabels:
+
+    def __init__(self, n_labels):
+        self.n_labels = n_labels
+        self.clfs = []
+        for i in range(0, self.n_labels):
+            clf = MLPClassifier(solver="lbfgs", hidden_layer_sizes=1000)
+            self.clfs.append(clf)
+        self.sampled_features = None
+        self.sampled_likelihoods = None
+
+    # クローン認識器の学習
+    #   (features, likelihoods): 訓練データ（特徴量と尤度ベクトルのペアの集合）
+    def fit(self, features, likelihoods):
+        self.sampled_features = features
+        self.sampled_likelihoods = likelihoods
+        labels = np.int32(likelihoods >= 0.5)  # 尤度0.5以上のラベルのみがターゲット認識器の認識結果であると解釈する
+        # Bool to Int
+        for i in range(0, self.n_labels):
+            l = labels[:, i]
+            self.clfs[i].fit(features, l)
+
+    # 未知の二次元特徴量を認識
+    #   features: 認識対象の二次元特徴量の集合
+    def predict_proba(self, features):
+        likelihoods = np.c_[np.zeros(features.shape[0])]
+        for i in trange(0, self.n_labels):
+            p = self.clfs[i].predict_proba(features)
+            likelihoods = np.hstack([likelihoods, np.c_[p[:, 1]]])
+        likelihoods = likelihoods[:, 1:]
+        return np.float32(self.correct_labels(features, likelihoods))
+
+    @staticmethod
+    def convert_to_px_arr(arr):
+        image_size = 512
+        return np.int32((arr + 1.0) * 0.5 * image_size)
+
+    def correct_labels(self, features, likelihoods):
+            features_px = self.convert_to_px_arr(features)
+            sampled_features_px = self.convert_to_px_arr(self.sampled_features)
+
+            for i, sampled in enumerate(sampled_features_px):
+                index_list = np.where(sampled == features_px)[0]
+                if len(index_list) != 0:
+                    likelihoods[index_list[0]] = self.sampled_likelihoods[i]
+
+            return likelihoods
